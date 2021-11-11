203d5c5442eeb0d74b7b387870a0fdf5569c3d03
Juliano Boese <juliano.boese@gmail.com>
2021-11-11 16:57:11 -0300

    Requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -63,8 +63,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbersArray) {
+  let fizzBuzzArray = [];
+  for (let number of numbersArray) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      fizzBuzzArray.push('fizzBuzz');
+    } else if (number % 3 === 0) {
+      fizzBuzzArray.push('fizz');
+    } else if (number % 5 === 0) {
+      fizzBuzzArray.push('buzz');
+    } else {
+      fizzBuzzArray.push('bug!');
+    }
+  }
+  return fizzBuzzArray;
 }
 
 // Desafio 9
