04ed47ec33ec33356e01245f4ed73d2eb10f5a66
Juliano Boese <juliano.boese@gmail.com>
2021-11-15 16:03:49 -0300

    Corrige função muito extensa no Requisito 9 / Remove variáveis desnecessárias nos requisitos 1 a 5 / Organiza referências

--- src/challenges.js
+++ src/challenges.js
@@ -1,39 +1,34 @@
 // Desafio 1
 function compareTrue(boolean1, boolean2) {
-  let result = false;
   if (boolean1 && boolean2) {
-    result = true;
+    return true;
   }
-  return result;
+  return false;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height) / 2;
-  return area;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(sentence) {
-  let sentenceArray = sentence.split(' ');
-  return sentenceArray;
+  return sentence.split(' ');
 }
 
 // Desafio 4
 function concatName(stringArray) {
-  let finalString = `${stringArray[stringArray.length - 1]}, ${stringArray[0]}`;
-  return finalString;
+  return `${stringArray[stringArray.length - 1]}, ${stringArray[0]}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let points = wins * 3 + ties;
-  return points;
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
 function highestCount(valuesArray) {
-  /** Para reduzir a complexidade da função e atender ao ESLint, utilizei a função Math.max.
+  /** Ref 1: Para reduzir a complexidade da função e atender ao ESLint, utilizei a função Math.max.
    * Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max#retornando_o_maior_elemento_de_um_array */
   let highest = Math.max(...valuesArray);
   let count = 0;
@@ -61,7 +56,6 @@ function highestCount(valuesArray) {
 //   }
 //   return count;
 // }
-
 // console.log(highestCount2([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
@@ -78,7 +72,7 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-// Função auxiliar "verifiFizzBuzz" criada para reduzir a complexidade da função "fizzBuzz".
+// Função auxiliar criada para reduzir a complexidade da função "fizzBuzz".
 function verifyFizzBuzz(number, fizz, buzz) {
   if (number % fizz === 0 && number % buzz === 0) {
     return 'fizzBuzz';
@@ -98,8 +92,6 @@ function fizzBuzz(numbersArray) {
   return fizzBuzzArray;
 }
 
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
-
 // Desafio 9
 function encode(stringToEncode) {
   let conversionObject = {
@@ -109,24 +101,26 @@ function encode(stringToEncode) {
     o: 4,
     u: 5,
   };
-
   let encodedString = stringToEncode;
   for (let key in conversionObject) {
-    /** Condicional criado conforme documentação do ESLint para viabilizar a utilização do "for in".
-     * Source: https://eslint.org/docs/rules/guard-for-in */
+    /** Ref 2: abaixo da função. */
     if ({}.hasOwnProperty.call(conversionObject, key)) {
-      /** Para reduzir a complexidade da função, utilizei o método "replace" com a flag "g" (global), para substituir todas as ocorrências, não somente a primeira.
-       * Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll
-       * Utilizei o objeto "new RegExp" para implementar o método "replace" com a flag "g".
-       * Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
-       */
+      /** Ref 3: abaixo da função. */
       let replacedLetter = new RegExp(key, 'g');
+      /** Ref 4: abaixo da função. */
       encodedString = encodedString.replace(replacedLetter, conversionObject[key]);
     }
   }
   return encodedString;
 }
+/** Ref 2: Condicional criado conforme documentação do ESLint para viabilizar a utilização do "for in".
+* Source: https://eslint.org/docs/rules/guard-for-in */
+/** Ref 3: Utilizei o objeto "new RegExp" para implementar o método "replace" com a flag "g".
+* Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp */
+/** Ref 4: Para reduzir a complexidade da função, utilizei o método "replace" com a flag "g" (global) para substituir todas as ocorrências sem necessidade de um loop.
+* Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll */
 
+// Versão alternativa sem utilização de funções/métodos consultados na internet, porém com complexidade acima de 5.
 // function encode2(stringToEncode) {
 //   let conversionArray = ['a', 'e', 'i', 'o', 'u'];
 //   let encodedString = stringToEncode;
@@ -139,7 +133,6 @@ function encode(stringToEncode) {
 //   }
 //   return encodedString;
 // }
-
 // console.log(encode2('hi there!'));
 
 function decode(stringToEncode) {
@@ -162,12 +155,13 @@ function decode(stringToEncode) {
   return decodedString;
 }
 
+// Versão alternativa sem utilização de funções/métodos consultados na internet, porém com complexidade acima de 5.
 // function decode2(stringToDecode) {
 //   let conversionArray = ['a', 'e', 'i', 'o', 'u'];
 //   let decodedString = stringToDecode;
 //   for (let i = 0; i < stringToDecode.length; i += 1) {
 //     for (let j = 0; j < conversionArray.length; j += 1) {
-//       /** Usei a função Number para poder usar o operador === em vez de ==
+//       /** Ref 5: Usei a função Number para poder usar o operador === em vez de ==
 //        * Source: https://stackoverflow.com/questions/1133770/how-to-convert-a-string-to-an-integer-in-javascript */
 //       if (Number(stringToDecode[i]) === j + 1) {
 //         decodedString = decodedString.replace(
@@ -179,7 +173,6 @@ function decode(stringToEncode) {
 //   }
 //   return decodedString;
 // }
-
 // console.log(decode2('h3 th2r2!'));
 
 module.exports = {
